09a7d869a1276626d7189a7c275bc5e64eaebb08
Regnars <k.regnars@gmail.com>
2024-09-20 14:23:05 +0300

    completed crud

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+route::apiResource('posts', PostController::class);
 
-route::get('/',function(){
-    return 'API';
-});
+// route::get('/',function(){
+//     return 'API';
+// });
